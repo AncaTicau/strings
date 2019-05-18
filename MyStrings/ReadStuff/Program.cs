b69083d5f0df391cf4608bb012596d723c6dc637
MariusA <anca.ticau@gmail.com>
2019-05-18 12:29:34 +0300

    Introduction to Strings

---
+++ MyStrings/ReadStuff/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ReadStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Please enter a string");
+
+            string readedLine = Console.ReadLine();
+
+            Console.WriteLine("Please enter a second string");
+
+            string readedLine2 = Console.ReadLine();
+
+            if (readedLine == readedLine2)
+            {
+                Console.WriteLine("They are the same");
+            }
+
+            //Compare
+
+            Console.WriteLine(string.Compare(readedLine, readedLine2, true));
+
+            if (readedLine.Equals(readedLine2))
+            {
+                Console.WriteLine("equals");
+            }
+
+            //Console.WriteLine(readedLine.Length/2F);
+
+        }
+    }
+}
---
+++ MyStrings/StringManipulation/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = " Ana are mere! ";
+
+            Console.WriteLine($"index of a : {myString.IndexOf('a')}");
+
+            Console.WriteLine($"last index of e : {myString.LastIndexOf('e')}");
+
+            //string mere = myString.Substring(0);
+
+            string mere = myString.Substring(9, 4);
+
+            string mere2 = myString.Substring(myString.IndexOf('m'), 4);
+
+            Console.WriteLine(mere);
+
+            Console.WriteLine(mere2);
+
+
+            string cleanedString = myString.Trim();
+
+            Console.WriteLine(cleanedString);
+
+
+            string cleanedStringStart = myString.TrimStart();
+
+            Console.WriteLine(cleanedStringStart);
+
+
+            string replacedString = myString.Replace(" ", ".");
+
+            Console.WriteLine(replacedString);
+
+            string replaceA = myString.Replace("a", "!", true, new CultureInfo("en-us"));
+
+            Console.WriteLine(replaceA);
+
+            //split
+
+            var splitted = myString.Trim().Split(new char[] { ' ' });
+
+            for (int i = 0; i < splitted.Length; i++)
+            {
+                Console.WriteLine(splitted[i]);
+            }
+
+            Console.WriteLine(myString.ToLower());
+            Console.WriteLine(myString.ToUpper());
+
+            Console.WriteLine(myString.Remove(9, 4));
+
+            //concatenation
+
+            string concatenated = "";
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                concatenated = concatenated + i.ToString();
+
+                //v2
+
+                //concatenated += i.ToString();
+            }
+
+            timer.Stop();
+
+            //Console.WriteLine(concatenated);
+            Console.WriteLine(timer.Elapsed);
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Stopwatch timerForBuilder = new Stopwatch();
+            timerForBuilder.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                stringBuilder.Append(i.ToString());
+            }
+
+            timerForBuilder.Stop();
+
+            //Console.WriteLine(stringBuilder);
+
+            Console.WriteLine(timerForBuilder.Elapsed);
+        }
+    }
+}
